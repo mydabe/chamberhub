f782a764ff8e91ca4b98103fa5e036a772a99d69
Myles Bell <myles.bell@duke.edu>
2025-09-26 21:09:01 -0700

    Created pages for credential sign in/up. Implemented buttons that route to them as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,48 @@
 import { signIn } from '@/auth'
+import Link from "next/link";
 
 export function SignIn() {
     return (
-        <form className={"text-center"}
-            action={async () => {
-                "use server"
-                await signIn("google", {redirectTo: "/dashboard"} )
-
-            }}
-        >
-            <button
-                type="submit"
-                className="bg-sky-500 text-white font-mono px-4 py-2 rounded-md ">
+        <div className="text-center">
+            {/* Sign in with Google */}
+            <form
+                action={async () => {
+                    "use server";
+                    await signIn("google", { redirectTo: "/dashboard" });
+                }}
+            >
+                <button
+                    type="submit"
+                    className="bg-sky-500 text-white font-mono px-4 py-2 rounded-md mt-20"
+                >
                     Sign in with Google
-            </button>
-        </form>
+                </button>
+            </form>
+            <Link
+                className="bg-sky-500 text-white font-mono px-4 py-2 rounded-md mt-20"
+                href="/cred_sign"
+            >
+                Sign In with E-Mail
+
+            </Link>
+
+        </div>
+    );
+}
+
+export function CreateAccount() {
+    return (
+        <div className="text-center">
+            <Link
+                href="/signup"
+                className="mt-4 text-sky-600"
+            >
+                Create an Account
+            </Link>
+        </div>
     )
 }
+
 
 export default function LoginScreen() {
   return (
@@ -29,8 +55,10 @@ export default function LoginScreen() {
         </h3>
         <div className="w-96 p-6 border rounded-lg mx-auto h-75">
             <SignIn/>
+            <div className= "mt-25">
+                <CreateAccount/>
+            </div>
         </div>
-
       </div>
 
   );
